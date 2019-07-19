03daf07944a0fcfb38fb412ac05ba49535b6a173
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2019-07-19 17:58:48 -0300

    authentication

--- app/Controllers/Http/Api/AuthController.js
+++ app/Controllers/Http/Api/AuthController.js
@@ -1,11 +1,29 @@
 'use strict';
 
+const User = use('UserModel');
+const Hash = use('Hash');
+
 class AuthController {
-  async token({ request, response }) {}
+  async token({ request, response, auth }) {
+    const { email, password } = request.all();
+    const user = await User.findBy({ email, is_active: true });
+    if (user) {
+      const isAuth = await Hash.verify(password, user.password);
+      if (isAuth) {
+        return await auth.withRefreshToken().generate(user);
+      } else {
+        return response.status(400).send({ message: 'Senha inválida' });
+      }
+    }
+    return response.status(400).send({ message: 'E-mail inválido' });
+  }
 
-  async forgotPassword({ request, response }) {}
+  async forgotPassword() {}
 
-  async me({ request, auth }) {}
+  async me({ auth }) {
+    const { id } = await auth.getUser();
+    return User.findOrFail(id);
+  }
 }
 
 module.exports = AuthController;
