03c3381d9ad5ff52fc0f1d2814e1647a7edbbf6a
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2019-09-06 09:44:57 -0300

    add auth/me endpoint

--- app/Controllers/Http/Api/AuthController.js
+++ app/Controllers/Http/Api/AuthController.js
@@ -7,15 +7,9 @@ class AuthController {
   async token({ request, response, auth }) {
     const { email, password } = request.all();
 
-    if (!email)
-      return response
-        .status(400)
-        .send({ message: 'Por favor, informe um e-mail' });
+    if (!email) return response.status(400).send({ message: 'Por favor, informe um e-mail' });
 
-    if (!password)
-      return response
-        .status(400)
-        .send({ message: 'Por favor, informe uma senha' });
+    if (!password) return response.status(400).send({ message: 'Por favor, informe uma senha' });
 
     const user = await User.findBy({ email });
     if (user) {
@@ -23,21 +17,16 @@ class AuthController {
       if (isAuth) {
         return await auth.withRefreshToken().generate(user);
       } else {
-        return response
-          .status(400)
-          .send({ message: 'E-mail e/ou senha inválidos' });
+        return response.status(400).send({ message: 'E-mail e/ou senha inválidos' });
       }
     }
-    return response
-      .status(400)
-      .send({ message: 'E-mail e/ou senha inválidos' });
+    return response.status(400).send({ message: 'E-mail e/ou senha inválidos' });
   }
 
   async forgotPassword() {}
 
   async me({ auth }) {
-    const { id } = await auth.getUser();
-    return User.findOrFail(id);
+    return auth.getUser();
   }
 }
 
