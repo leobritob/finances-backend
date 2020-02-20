46c39735b44b38062d9f20c82ce34a9e0200b5a2
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2020-02-20 09:23:58 -0300

    feat: statehook

--- app/Models/Hooks/StateHook.js
+++ app/Models/Hooks/StateHook.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const { validateAll } = use('Validator');
+const ValidationException = use('App/Exceptions/ValidationException');
+
 const StateHook = (exports = module.exports = {});
 
-StateHook.validate = async modelInstance => {};
+StateHook.validate = async modelInstance => {
+  const rules = {
+    name: 'required|min:1|max:255',
+    uf: 'required|min:2|max:2',
+    country_id: 'required|exists:countries,id',
+  };
+
+  const messages = {
+    'name.required': 'Por favor, informe um nome.',
+    'name.min': 'O nome precisa ter no mínimo 2 caracteres.',
+    'name.max': 'O nome atingiu limite máximo de 255 caracteres.',
+    'uf.required': 'Por favor, informe a UF.',
+    'uf.min': 'A versão traduzida do nome precisa ter no mínimo 2 caracteres.',
+    'uf.max': 'A versão traduzida do nome atingiu limite máximo de 2 caracteres.',
+    'country_id:required': 'Por favor, informe o país.',
+    'country_id:existss': 'O país informado não existe.',
+  };
+
+  const validation = await validateAll(modelInstance.$attributes, rules, messages);
+  if (validation.fails()) throw new ValidationException(validation.messages());
+};
