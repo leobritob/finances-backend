98b828718423a7a379655a2292ed543ab03f261d
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2020-02-20 20:27:08 -0300

    feat: improve customers migration

--- database/migrations/1582240830909_customers_schema.js
+++ database/migrations/1582240830909_customers_schema.js
@@ -9,11 +9,16 @@ class CustomersSchema extends Schema {
       table.string('first_name').notNullable();
       table.string('last_name').notNullable();
       table.date('birthday_date').nullable();
+      table.string('gender').notNullable();
+      table.string('profession').notNullable();
+      table.string('cellphone').notNullable();
+      table.string('phone').notNullable();
       table.string('cpf').notNullable();
       table.string('rg').notNullable();
       table.string('street_name').nullable();
       table.string('street_number').nullable();
       table.string('district').nullable();
+      table.string('zipcode').nullable();
       table
         .integer('city_id')
         .nullable()
